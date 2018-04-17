6de0688b0343beb3dba2b45ff472272e9876a32f
joncancode <jc.haines19@gmail.com>
2018-04-16 22:44:19 -0400

    add router, redirects, and placeholders for shapes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+const Circle = () => {
+  return (
+    <div>
+      <svg height="200" width="200">
+        <circle cx="50" cy="50" r="40" />
+      </svg>
+    </div>
+  );
+};
+
+const Square = () => {
+  return (
+    <div>
+      <svg height="200" width="200">
+        <rect width="50" height="50" />
+      </svg>
+    </div>
+  );
+};
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404: Page Not Found</h1>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="container">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <Redirect to="/shapes/circles" />}
+          />
+
+          <Route exact path="/shapes/circles" component={Circle} />
+
+          <Route exact path="/shapes/squares" component={Square} />
+
+          <Route exact path="*" component={NotFound} />
+          </Switch>
+          
+        </div>
+      </Router>
     );
   }
 }
